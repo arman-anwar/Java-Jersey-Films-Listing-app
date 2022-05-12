abddee0c584d34ac46eff8bde3191fed71097ab4
Arman <arman.anwar@gmail.com>
2022-05-12 13:00:37 +0100

    code cleanup

--- src/main/webapp/films-form.jsp
+++ src/main/webapp/films-form.jsp
@@ -7,7 +7,7 @@
 <div class="container col-md-5">
 	<div class="card">
 		<div class="card-body">
-			<form name="myForm" onsubmit="return validateForm();" id="myForm">
+			<form name="myForm"  id="myForm">
 				<caption>
 					<h2>Film Data</h2>
 				</caption>
@@ -84,72 +84,6 @@
 						})
 
 					});
-
-	function validateForm() {
-	//	let x = document.forms["myForm"]["isbn"].value;
-	//	if (!isValidIsbn(x)) {
-	//		alert("Invalid ISBN");
-		//	return false;
-//		}
-	}
-	(function() {
-		'use strict'
-
-		// Fetch all the forms we want to apply custom Bootstrap validation styles to
-		var forms = document.querySelectorAll('.needs-validation')
-
-		// Loop over them and prevent submission
-		Array.prototype.slice.call(forms).forEach(function(form) {
-			form.addEventListener('submit', function(event) {
-				if (!form.checkValidity()) {
-					event.preventDefault()
-					event.stopPropagation()
-				}
-
-				form.classList.add('was-validated')
-			}, false)
-		})
-	})()
-
-	var isValidIsbn = function(str) {
-
-		var sum, weight, digit, check, i;
-
-		str = str.replace(/[^0-9X]/gi, '');
-
-		if (str.length != 10 && str.length != 13) {
-			return false;
-		}
-
-		if (str.length == 13) {
-			sum = 0;
-			for (i = 0; i < 12; i++) {
-				digit = parseInt(str[i]);
-				if (i % 2 == 1) {
-					sum += 3 * digit;
-				} else {
-					sum += digit;
-				}
-			}
-			check = (10 - (sum % 10)) % 10;
-			return (check == str[str.length - 1]);
-		}
-
-		if (str.length == 10) {
-			weight = 10;
-			sum = 0;
-			for (i = 0; i < 9; i++) {
-				digit = parseInt(str[i]);
-				sum += weight * digit;
-				weight--;
-			}
-			check = (11 - (sum % 11)) % 11
-			if (check == 10) {
-				check = 'X';
-			}
-			return (check == str[str.length - 1].toUpperCase());
-		}
-	}
 
 	function isNumber(evt) {
 		evt = (evt) ? evt : window.event;
--- src/main/webapp/static/script.js
+++ src/main/webapp/static/script.js
@@ -35,12 +35,6 @@ $(document).ready(function() {
 
 });
 
-$("button").click(function() {
-	event.preventDefault();
-
-
-});
-
 $("#myForm").submit(function(event) {
 	event.preventDefault();
 	alert("Handler for .submit() called.");
@@ -67,80 +61,3 @@ $("#myForm").submit(function(event) {
 
 
 });
-
-
-function validateForm() {
-	let x = document.forms["myForm"]["isbn"].value;
-	if (!isValidIsbn(x)) {
-		alert("Invalid ISBN");
-		return false;
-	}
-}
-(function() {
-	'use strict'
-
-	// Fetch all the forms we want to apply custom Bootstrap validation styles to
-	var forms = document.querySelectorAll('.needs-validation')
-
-	// Loop over them and prevent submission
-	Array.prototype.slice.call(forms).forEach(function(form) {
-		form.addEventListener('submit', function(event) {
-			if (!form.checkValidity()) {
-				event.preventDefault()
-				event.stopPropagation()
-			}
-
-			form.classList.add('was-validated')
-		}, false)
-	})
-})()
-
-var isValidIsbn = function(str) {
-
-	var sum, weight, digit, check, i;
-
-	str = str.replace(/[^0-9X]/gi, '');
-
-	if (str.length != 10 && str.length != 13) {
-		return false;
-	}
-
-	if (str.length == 13) {
-		sum = 0;
-		for (i = 0; i < 12; i++) {
-			digit = parseInt(str[i]);
-			if (i % 2 == 1) {
-				sum += 3 * digit;
-			} else {
-				sum += digit;
-			}
-		}
-		check = (10 - (sum % 10)) % 10;
-		return (check == str[str.length - 1]);
-	}
-
-	if (str.length == 10) {
-		weight = 10;
-		sum = 0;
-		for (i = 0; i < 9; i++) {
-			digit = parseInt(str[i]);
-			sum += weight * digit;
-			weight--;
-		}
-		check = (11 - (sum % 11)) % 11
-		if (check == 10) {
-			check = 'X';
-		}
-		return (check == str[str.length - 1].toUpperCase());
-	}
-}
-
-function isNumber(evt) {
-	evt = (evt) ? evt : window.event;
-	var charCode = (evt.which) ? evt.which : evt.keyCode;
-	if ((charCode > 31 && charCode < 48) || charCode > 57) {
-		return false;
-	}
-	return true;
-}
-
